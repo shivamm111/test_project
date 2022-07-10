5e9e4a37b33d05998a1720b19a13f24e0c390764
Shivam17 <shivmehta1710@gmail.com>
2022-07-10 18:21:11 +0530

    added

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const _ = require('loadsh')
-const items = [1,[2,[3,[4]]]]
-const newItems = _.flattenDeep(items)
+const item = [1,[2,[3,[4]]]]
+const newItems = _.flattenDeep(item)
 
 console.log(newItems);
